4806c1e15b0c5be662d4579404df5df015338095
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2023-01-23 18:30:14 +0000

    adding verify endpoint and signin

--- server/dist/index.js
+++ server/dist/index.js
@@ -16,11 +16,15 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const client_1 = require("@prisma/client");
 const zod_1 = require("zod");
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const saltRounds = 10;
 const prisma = new client_1.PrismaClient();
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const port = process.env.PORT || 2000;
+const token_secret = process.env.TOKEN_SECRET || '';
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
@@ -28,7 +32,7 @@ app.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const users = yield prisma.user.findMany();
     res.send(users);
 }));
-app.post('/user', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/signup', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const body = req.body;
     const userSchema = zod_1.z.object({
         firstname: zod_1.z.string().min(2).max(20),
@@ -39,11 +43,52 @@ app.post('/user', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     }).required();
     const valid = userSchema.safeParse(body);
     if (valid.success) {
-        res.json({ requestBody: req.body });
+        bcrypt_1.default.hash(body.password, saltRounds, function (err, hash) {
+            return __awaiter(this, void 0, void 0, function* () {
+                yield prisma.user.create({ data: Object.assign(Object.assign({}, body), { password: hash }) });
+                const token = jsonwebtoken_1.default.sign({ foo: 'bar' }, token_secret, {});
+                res.json({
+                    requestBody: body,
+                    token
+                });
+            });
+        });
     }
     else {
         res.json({ error: valid.error });
     }
+}));
+app.post('/signin', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const body = req.body;
+    const userSchema = zod_1.z.object({
+        username: zod_1.z.string().min(2).max(20),
+        password: zod_1.z.string()
+    }).required();
+    const valid = userSchema.safeParse(body);
+    if (valid.success) {
+        const user = yield prisma.user.findUnique({
+            where: {
+                username: body.username
+            }
+        });
+        if (!user || !user.password) {
+            res.json('F');
+        }
+        bcrypt_1.default.compare(body.password, user.password, function (err, result) {
+            if (result) {
+                const token = jsonwebtoken_1.default.sign({ user_id: user === null || user === void 0 ? void 0 : user.id }, token_secret, {});
+                res.json({ token });
+            }
+        });
+    }
+}));
+app.post('/verify', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const authToken = req.get('Authorization');
+    if (authToken) {
+        const decodedToken = jsonwebtoken_1.default.verify(authToken, token_secret);
+        res.json(decodedToken);
+    }
+    res.json('invalid token fr');
 }));
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,9 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client'
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+const saltRounds = 10;
 
 const prisma = new PrismaClient()
 
@@ -10,6 +13,7 @@ dotenv.config();
 const app: Express = express();
 app.use(express.json());
 const port = process.env.PORT || 2000;
+const token_secret = process.env.TOKEN_SECRET || '';
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
@@ -20,7 +24,7 @@ app.get('/users', async (req: Request, res: Response) => {
   res.send(users);
 });
 
-app.post('/user', async (req: Request, res: Response) => {
+app.post('/signup', async (req: Request, res: Response) => {
   const body = req.body;
   const userSchema = z.object({
       firstname: z.string().min(2).max(20),
@@ -33,11 +37,61 @@ app.post('/user', async (req: Request, res: Response) => {
   const valid = userSchema.safeParse(body);
 
   if (valid.success) {
-    res.json({requestBody: req.body})
+    bcrypt.hash(body.password, saltRounds, async function(err, hash) {
+      await prisma.user.create({data: {...body, password: hash}});
+      const token = jwt.sign({foo: 'bar'}, token_secret, {})
+      res.json(
+        {
+          requestBody: body,
+          token
+        }
+      )
+    });
   } else {
     res.json({error: valid.error})
   }
+});
 
+app.post('/signin', async (req: Request, res: Response) => {
+  const body = req.body;
+  const userSchema = z.object({
+      username: z.string().min(2).max(20),
+      password: z.string()
+    }).required();
+
+  const valid = userSchema.safeParse(body);
+
+  if (valid.success) {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: body.username
+      }
+    });
+
+    if (!user || !user.password) {
+      res.json('F');
+    }
+
+    bcrypt.compare(body.password, user.password, function(err, result) {
+      if (result) {
+        const token = jwt.sign({user_id: user?.id}, token_secret, {})
+
+        res.json({token});
+      }
+    })
+  }
+
+});
+
+app.post('/verify', async (req: Request, res: Response) => {
+  const authToken = req.get('Authorization');
+
+  if (authToken) {
+    const decodedToken = jwt.verify(authToken, token_secret) as any
+    res.json(decodedToken)
+  }
+
+  res.json('invalid token fr');
 });
 
 app.listen(port, () => {
