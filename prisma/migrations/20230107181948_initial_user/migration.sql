3b6bba0cb0180810d86d8aa60175a132a1caa038
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2023-01-27 19:14:46 +0000

    moving prisma to server file

--- prisma/migrations/20230107181948_initial_user/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "joined" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "firstname" TEXT NOT NULL,
-    "lastname" TEXT NOT NULL
-);
