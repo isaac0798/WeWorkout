0abd721c8ce31fbe1f7d9dc918ed41417852ce40
Isaac Tennant <isaactennant@isaacs-mbp.home>
2024-07-06 12:24:16 +0100

    add calender

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,31 @@
+import { useState } from 'react'
 import type { User } from '@supabase/supabase-js'
 import type { GetServerSidePropsContext } from 'next'
 
 import { createClient } from '@/utils/supabase/server-props'
-
+import { Calendar } from '@/components/ui/calendar'
 import Page from '@/components/page'
 import Section from '@/components/section'
 
-const Index = ({ user }: { user: User }) => (
-	<Page>
-		<Section>
-			<h1>Hello, {user.email || 'user'}!</h1>
-			<h2 className='text-xl font-semibold text-zinc-800 dark:text-zinc-200'>
-				We grow a lot of rice.
-			</h2>
+const Index = ({ user }: { user: User }) => {
+	const [date, setDate] = useState<Date | undefined>(new Date())
+	console.log(date)
 
-			<div className='mt-2'>
-				<p className='text-zinc-600 dark:text-zinc-400'>
-					You love rice, and so does the rest of the world. In the crop year
-					2008/2009, the milled rice production volume amounted to over{' '}
-					<span className='font-medium text-zinc-900 dark:text-zinc-50'>
-						448 million tons
-					</span>{' '}
-					worldwide.
-				</p>
-
-				<br />
-
-				<p className='text-sm text-zinc-600 dark:text-zinc-400'>
-					<a
-						href='https://github.com/mvllow/next-pwa-template'
-						className='underline'
-					>
-						Source
-					</a>
-				</p>
-			</div>
-		</Section>
-	</Page>
-)
+	return (
+		<Page>
+			<Section>
+				<h1>Hello, {user.email || 'user'}!</h1>
+				return (
+				<Calendar
+					mode='single'
+					selected={date}
+					onSelect={setDate}
+					className='rounded-md border'
+				/>
+			</Section>
+		</Page>
+	)
+}
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
 	const supabase = createClient(context)
