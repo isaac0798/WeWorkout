0944490c7ae009c273f202387a736193998c34ca
Isaac Tennant <isaactennant@isaacs-mbp.home>
2024-07-06 11:05:54 +0100

    add auth forrealsies

--- components/page.tsx
+++ components/page.tsx
@@ -11,7 +11,7 @@ const Page = ({ title, children }: Props) => (
 	<>
 		{title ? (
 			<Head>
-				<title>Rice Bowl | {title}</title>
+				<title>WeWorkout | {title}</title>
 			</Head>
 		) : null}
 
