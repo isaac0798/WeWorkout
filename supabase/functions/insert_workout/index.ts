a012e2451bdadace9e8fe76bbcf46f65ac00d903
Isaac Tennant <isaactennant@Isaacs-MacBook-Pro.local>
2024-07-11 15:56:08 +0100

    saving workout

--- supabase/functions/insert_workout/index.ts
+++ supabase/functions/insert_workout/index.ts
@@ -20,8 +20,8 @@ interface WorkoutData {
 Deno.serve(async (req) => {
 	// Create a Supabase client with the Admin key
 	const supabaseClient = createClient(
-		Deno.env.get('SUPABASE_URL') ?? '',
-		Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
+		Deno.env.get('http://127.0.0.1:54321') ?? '',
+		Deno.env.get('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU') ?? '',
 	)
 
 	if (req.method === 'OPTIONS') {
