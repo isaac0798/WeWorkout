58d019461115d7665322fbaac140fff9ef41bd9a
Isaac Tennant <isaactennant@Isaacs-MacBook-Pro.local>
2024-07-22 18:27:16 +0100

    templates

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -21,8 +21,8 @@ const Appbar = () => {
 										href={href}
 										className={`text-sm ${
 											router.pathname === href
-												? 'text-indigo-500 dark:text-indigo-400'
-												: 'text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50'
+												? "text-indigo-500 dark:text-indigo-400"
+												: "text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50"
 										}`}
 									>
 										{label}
@@ -39,24 +39,23 @@ const Appbar = () => {
 
 const links = [
 	{
-		label: 'Templates',
-		href: '/templates',
+		label: "Templates",
+		href: "/templates",
 		icon: (
 			<svg
-				viewBox='0 0 15 15'
-				fill='none'
-				xmlns='http://www.w3.org/2000/svg'
-				width='18'
-				height='18'
+				viewBox="0 0 15 15"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				width="18"
+				height="18"
 			>
 				<path
-					d='M7.5.5l.325-.38a.5.5 0 00-.65 0L7.5.5zm-7 6l-.325-.38L0 6.27v.23h.5zm5 8v.5a.5.5 0 00.5-.5h-.5zm4 0H9a.5.5 0 00.5.5v-.5zm5-8h.5v-.23l-.175-.15-.325.38zM1.5 15h4v-1h-4v1zm13.325-8.88l-7-6-.65.76 7 6 .65-.76zm-7.65-6l-7 6 .65.76 7-6-.65-.76zM6 14.5v-3H5v3h1zm3-3v3h1v-3H9zm.5 3.5h4v-1h-4v1zm5.5-1.5v-7h-1v7h1zm-15-7v7h1v-7H0zM7.5 10A1.5 1.5 0 019 11.5h1A2.5 2.5 0 007.5 9v1zm0-1A2.5 2.5 0 005 11.5h1A1.5 1.5 0 017.5 10V9zm6 6a1.5 1.5 0 001.5-1.5h-1a.5.5 0 01-.5.5v1zm-12-1a.5.5 0 01-.5-.5H0A1.5 1.5 0 001.5 15v-1z'
-					fill='currentColor'
+					d="M7.5.5l.325-.38a.5.5 0 00-.65 0L7.5.5zm-7 6l-.325-.38L0 6.27v.23h.5zm5 8v.5a.5.5 0 00.5-.5h-.5zm4 0H9a.5.5 0 00.5.5v-.5zm5-8h.5v-.23l-.175-.15-.325.38zM1.5 15h4v-1h-4v1zm13.325-8.88l-7-6-.65.76 7 6 .65-.76zm-7.65-6l-7 6 .65.76 7-6-.65-.76zM6 14.5v-3H5v3h1zm3-3v3h1v-3H9zm.5 3.5h4v-1h-4v1zm5.5-1.5v-7h-1v7h1zm-15-7v7h1v-7H0zM7.5 10A1.5 1.5 0 019 11.5h1A2.5 2.5 0 007.5 9v1zm0-1A2.5 2.5 0 005 11.5h1A1.5 1.5 0 017.5 10V9zm6 6a1.5 1.5 0 001.5-1.5h-1a.5.5 0 01-.5.5v1zm-12-1a.5.5 0 01-.5-.5H0A1.5 1.5 0 001.5 15v-1z"
+					fill="currentColor"
 				/>
 			</svg>
 		),
 	},
-]
-
+];
 
 export default Appbar;
--- pages/templates.tsx
+++ pages/templates.tsx
@@ -1,18 +1,90 @@
-import Page from '@/components/page'
-import { createClient } from '@/utils/supabase/static-props'
+import Page from "@/components/page";
+import { Button } from "@/components/ui/button";
+import { createFEClient } from "@/utils/supabase/component";
+import { createClient } from "@/utils/supabase/server-props";
+import { GetServerSideProps, type GetServerSidePropsContext } from "next";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import type { Exercise } from ".";
 
-export default function PublicPage({ data }: { data?: any[] }) {
-	return <Page>{data && JSON.stringify(data, null, 2)}</Page>
+export interface Template {
+	id: string;
+	name: string;
+	exercise: Exercise[];
 }
 
-export async function getStaticProps() {
-	const supabase = createClient()
+const TEMPLATES: Template[] = [];
 
-	const { data, error } = await supabase.from('countries').select()
+const defaultTemplate = {
+	name: "New Workout",
+	exercises: [],
+};
+
+export default function PublicPage({ data }: { data?: any[] }) {
+	const supabase = createFEClient();
+
+	const [allExercises, setAllExercises] = useState<
+		{ id: string; name: string }[]
+	>([]);
+
+	const [templates, setTemplates] = useState(TEMPLATES);
+
+	useEffect(() => {
+		supabase
+			.from("Exercise")
+			.select("id, name")
+			.order("name")
+			.then(({ data, error }) => {
+				if (error) {
+					console.error("Error fetching exercises:", error);
+					throw error;
+				}
+				setAllExercises(data);
+			});
+	}, []);
+
+	return (
+		<Page>
+			<h1>Templates</h1>
+			<Button
+				onClick={() => {
+					setTemplates([
+						...templates,
+						{
+							id: uuidv4(),
+							name: "New Template",
+							exercise: [],
+						},
+					]);
+				}}
+			>
+				Add New Template
+			</Button>
+			<p>Existing Templates:</p>
+			{TEMPLATES.map((template, i) => {
+				return <div>Template {i}</div>;
+			})}
+		</Page>
+	);
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+	const supabase = createClient(context);
+
+	const { data, error } = await supabase.auth.getUser();
 
 	if (error || !data) {
-		return { props: {} }
+		return {
+			redirect: {
+				destination: "/login",
+				permanent: false,
+			},
+		};
 	}
 
-	return { props: { data } }
+	return {
+		props: {
+			user: data.user,
+		},
+	};
 }
