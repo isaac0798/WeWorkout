fb902e64ee4e1db1ee8f9d70a0926b3c7b741ef8
Isaac Tennant <isaactennant@Isaacs-MacBook-Pro.local>
2024-07-26 12:15:15 +0100

    show highest weight for reps for an exericse

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,53 +1,74 @@
-import Page from '@/components/page'
-import { createFEClient } from '@/utils/supabase/component'
-import { createClient } from '@/utils/supabase/server-props'
-import { User } from '@supabase/supabase-js'
-import { GetServerSidePropsContext } from 'next'
-import { useEffect, useState } from 'react'
+import Page from "@/components/page";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { createFEClient } from "@/utils/supabase/component";
+import { createClient } from "@/utils/supabase/server-props";
+import type { User } from "@supabase/supabase-js";
+import type { GetServerSidePropsContext } from "next";
+import { useEffect, useState } from "react";
 
 interface Workout {
-	id: string
-	name: string
-	date: string
+	id: string;
+	name: string;
+	date: string;
 	exercises: Array<{
-		id: string
-		name: string
+		id: string;
+		name: string;
 		sets: Array<{
-			id: string
-			weight: number
-			reps: number
-			completed: boolean
-		}>
-	}>
+			id: string;
+			weight: number;
+			reps: number;
+			completed: boolean;
+		}>;
+	}>;
+}
+
+interface Exercise {
+	id: string;
+	name: string;
+	sets: {
+		id: string;
+		weight: number;
+		reps: number;
+		completed: boolean;
+	}[];
 }
 
 const Index = ({ user }: { user: User }) => {
-	const supabase = createFEClient()
+	const supabase = createFEClient();
 	const [profile, setProfile] = useState<{
-		id: string
-		first_name: string
-    last_name: string
+		id: string;
+		first_name: string;
+		last_name: string;
 	}>();
 
-  const [workouts, setWorkouts] = useState<Workout[]>([])
+	const [workouts, setWorkouts] = useState<Workout[]>([]);
+	const [uniqueExercises, setUniqueExericses] = useState<Exercise[]>([]);
+	const [selectedExercise, setSelectedExercise] = useState<Exercise>();
+	console.log(selectedExercise);
 
 	useEffect(() => {
 		supabase
-			.from('profiles')
+			.from("profiles")
 			.select()
-			.eq('id', user.id)
-      .single()
+			.eq("id", user.id)
+			.single()
 			.then(({ data, error }) => {
 				if (error) {
-					console.error('error fetching profile:', error)
+					console.error("error fetching profile:", error);
 				}
 
-				setProfile(data)
-			})
+				setProfile(data);
+			});
 
-    const fetchUserWorkouts = async (userId: string) => {
+		const fetchUserWorkouts = async (userId: string) => {
 			const { data, error } = await supabase
-				.from('Workout')
+				.from("Workout")
 				.select(
 					`
       id,
@@ -68,12 +89,12 @@ const Index = ({ user }: { user: User }) => {
       )
     `,
 				)
-				.eq('user_id', userId)
-				.order('date', { ascending: false })
+				.eq("user_id", userId)
+				.order("date", { ascending: false });
 
 			if (error) {
-				console.error('Error fetching workouts:', error)
-				throw error
+				console.error("Error fetching workouts:", error);
+				throw error;
 			}
 
 			// Restructure the data for easier use
@@ -86,20 +107,41 @@ const Index = ({ user }: { user: User }) => {
 					name: we.Exercise.name,
 					sets: we.Sets,
 				})),
-			}))
+			}));
 
-			return formattedWorkouts
-		}
+			return formattedWorkouts;
+		};
 
 		// Usage
 		try {
 			fetchUserWorkouts(user.id).then((workouts) => {
-        setWorkouts(workouts)
-      })
+				setWorkouts(workouts);
+				setUniqueExericses(getUniqueExercises(workouts));
+			});
 		} catch (error) {
-			console.error('Failed to fetch workouts:', error)
+			console.error("Failed to fetch workouts:", error);
 		}
-	}, [])
+	}, []);
+
+	function getUniqueExercises(workouts: Workout[]): Exercise[] {
+		const exerciseSet = new Set<string>();
+		const uniqueExercises: Exercise[] = [];
+
+		workouts.forEach((workout) => {
+			workout.exercises.forEach((exercise) => {
+				if (!exerciseSet.has(exercise.id)) {
+					exerciseSet.add(exercise.id);
+					uniqueExercises.push({
+						id: exercise.id,
+						name: exercise.name,
+						sets: exercise.sets,
+					});
+				}
+			});
+		});
+
+		return uniqueExercises;
+	}
 
 	return (
 		<Page>
@@ -107,50 +149,115 @@ const Index = ({ user }: { user: User }) => {
 				{profile?.first_name} {profile?.last_name}
 			</h1>
 			<div>
-				{workouts.map((workout) => (
-					<div key={workout.id}>
-						<h2>
-							{workout.name} - {workout.date}
-						</h2>
-						{workout.exercises.map((exercise) => (
-							<div key={exercise.id}>
-								<h3>{exercise.name}</h3>
-								<ul>
-									{exercise.sets.map((set) => (
-										<li key={set.id}>
-											{set.weight}kg x {set.reps} reps
-											{set.completed ? ' (Completed)' : ''}
-										</li>
-									))}
-								</ul>
-							</div>
-						))}
+				<h1>Rep Max Finder: </h1>
+				<Select
+					onValueChange={(value) =>
+						setSelectedExercise(
+							uniqueExercises.find((exercise) => value === exercise.id),
+						)
+					}
+				>
+					<SelectTrigger>
+						<SelectValue placeholder="Pick an Exercise" />
+					</SelectTrigger>
+					<SelectContent>
+						{uniqueExercises.map((exercise) => {
+							return (
+								<SelectItem value={exercise.id}>{exercise.name}</SelectItem>
+							);
+						})}
+					</SelectContent>
+				</Select>
+				{/*         {getUniqueExercises(workouts).map((exercise) => {
+          console.log(exercise)
+          return (
+             <Select
+              value={exercise.id}
+              key={exercise.id}
+              onValueChange={(value) => {
+                console.log(value)
+              }}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder={exercise.name}/>
+              </SelectTrigger>
+              <SelectContent>
+                 <Select
+                  value={String(Math.min(...exercise.sets.map(set => set.reps)))}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder={String(Math.min(...exercise.sets.map(set => set.reps)))} />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {exercise.sets.map(set => set.reps).filter(function(item, pos, self) {
+                      return self.indexOf(item) == pos;
+                    }).map((rep) => {
+                      return (
+                        <SelectItem value={String(rep)} key={rep}>
+                          {rep}
+                        </SelectItem>
+                      )
+                    })}
+                  </SelectContent>
+                 </Select>
+              </SelectContent>
+            </Select>
+          )
+        })} */}
+				<h1>Reps:</h1>
+				{selectedExercise && (
+					<div>
+						{!selectedExercise.sets.length && (
+							<div>No Sets found for this exercise</div>
+						)}
+						{Object.entries(
+							selectedExercise.sets.reduce((acc, set) => {
+								if (set.reps in acc) {
+									if (set.weight > acc[set.reps]) {
+										acc[set.reps] = set.weight;
+
+										return acc;
+									}
+								} else {
+									acc[set.reps] = set.weight || 0;
+								}
+
+								return acc;
+							}, {}),
+						).map(([rep, weight]) => {
+							return (
+								<div className="flex flex-col">
+									<div>Rep: {rep}</div>
+									<div>Weight: {weight}</div>
+								</div>
+							);
+						})}
 					</div>
-				))}
+				)}
 			</div>
 		</Page>
-	)
-}
+	);
+};
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-	const supabase = createClient(context)
+	const supabase = createClient(context);
 
-	const { data, error } = await supabase.auth.getUser()
+	const { data, error } = await supabase.auth.getUser();
 
 	if (error || !data) {
 		return {
 			redirect: {
-				destination: '/login',
+				destination: "/login",
 				permanent: false,
 			},
-		}
+		};
 	}
 
 	return {
 		props: {
 			user: data.user,
 		},
-	}
+	};
 }
 
-export default Index
+export default Index;
